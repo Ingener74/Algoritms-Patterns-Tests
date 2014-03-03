207f969e48660f67830d2969c98501e64643d1b0
Pavel <shnaiderpasha@gmail.com>
2014-03-03 10:04:15 +0600

    asio client and server

--- apps/test_boost_asio/asio_client_001/src/main.cpp
+++ apps/test_boost_asio/asio_client_001/src/main.cpp
@@ -8,17 +8,20 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
-int main(int argc, char **argv) {
+void connect_handler(const boost::system::error_code&);
 
+int main(int argc, char **argv) {
     std::cout << "test asio client" << std::endl;
 
     using namespace boost::asio;
-
     io_service service;
-
+    ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 2001);
+    ip::tcp::socket sock(service);
+    sock.async_connect(ep, connect_handler);
+    service.run();
 
     return 0;
 }
 
-
-
+void connect_handler(const boost::system::error_code& ec){
+}
--- apps/test_boost_asio/asio_server_001/src/main.cpp
+++ apps/test_boost_asio/asio_server_001/src/main.cpp
@@ -6,19 +6,46 @@
  */
 
 #include <iostream>
+
+#include <boost/bind.hpp>
+#include <boost/local_function.hpp>
 #include <boost/asio.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/thread.hpp>
+
+using namespace boost::asio;
+using namespace boost::local_function;
+
+typedef boost::shared_ptr<ip::tcp::socket> socket_ptr;
+
+void client_session(socket_ptr);
 
 int main(int argc, char **argv) {
     std::cout << "test asio server" << std::endl;
 
-    using namespace boost::asio;
     io_service service;
-    ip::tcp::endpoint ep(ip::address::from_string("128.0.0.1"), 2001);
-    ip::tcp::socket sock(service);
-    sock.connect(ep);
+    ip::tcp::endpoint endp(ip::tcp::v4(), 2001);
+    ip::tcp::acceptor acc(service, endp);
+    while(1){
+        socket_ptr sock(new ip::tcp::socket(service));
+        acc.accept(*sock);
+
+//        int BOOST_LOCAL_FUNCTION(void) {
+//            return 0;
+//        } BOOST_LOCAL_FUNCTION(test_thread)
+
+        boost::thread(boost::bind(client_session, sock));
+    }
 
     return 0;
 }
 
-
-
+void client_session(socket_ptr sock){
+    while(true){
+        char data[512];
+        size_t len = sock->read_some(buffer(data));
+        if(len > 0){
+            write(*sock, buffer("ok", 2));
+        }
+    }
+}
