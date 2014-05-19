d9e4ee154fef8a878207f6806ce07d55ae92a3b9
Shnayder Pavel <pavel_shnayder@3divi.com>
2014-05-19 09:19:34 +0600

    added test_externC test library added resources for myqt

---
+++ modules/test_externC/src/Test.cpp
@@ -0,0 +1,17 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: May 19, 2014
+ *      Author: pavel
+ */
+
+const char* returnString()
+{
+    return "Return string: Test extern C";
+}
+
+int returnInt()
+{
+    return 28;
+}
+
---
+++ modules/test_externC/src/Test.h
@@ -0,0 +1,20 @@
+/*
+ * Test.h
+ *
+ *  Created on: May 19, 2014
+ *      Author: pavel
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+extern "C"
+{
+
+const char * returnString();
+
+int returnInt();
+
+}
+
+#endif /* TEST_H_ */
