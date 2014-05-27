a4e648a61928e4d526ba3db52fe4b1df1988fdc6
Shnayder Pavel <pavel_shnayder@3divi.com>
2014-05-27 09:28:07 +0600

    now work

--- apps/highload/NOK_reader/main.cpp
+++ apps/highload/NOK_reader/main.cpp
@@ -10,7 +10,9 @@
 #include <mutex>
 #include <string>
 #include <thread>
+#include <vector>
 #include <memory>
+#include <future>
 #include <cstdint>
 #include <sstream>
 #include <iostream>
@@ -19,37 +21,97 @@
 
 using namespace std;
 
+/*
+ * expand to prime numbers
+ */
+vector<uint64_t> expand(uint64_t n)
+{
+    vector<uint64_t> multipliers;
+    uint64_t m = 2;
+    while (n != 1)
+    {
+        if (n % m)
+        {
+            if (++m == n)
+            {
+                multipliers.push_back(m);
+                break;
+            }
+        }
+        else
+        {
+            n /= m;
+            multipliers.push_back(m);
+            m = 2;
+        }
+    }
+    return multipliers;
+}
+
 int main(int argc, char **argv)
 {
     try
     {
-        deque<uint64_t> md; // main deque
+        cout << "lcm finder" << endl;
+
+        /*
+         * main deque
+         */
+        deque<uint64_t> md;
         mutex mdm;
         condition_variable mdc;
+        /*
+         * stop flag
+         */
+        bool stop = false;
 
+        /*
+         * number of threads
+         */
         const int N = 4;
-        thread threads[N];
 
-        for (auto &t : threads)
+        /*
+         * futures for async tasks
+         */
+        future<map<uint64_t, uint64_t>> futures[N];
+
+        for (auto &f : futures)
         {
-            t = thread([&]()
+            /*
+             * creating async tasks
+             */
+            f = async(launch::async, [&]()
             {
-                for(;;){
+                map<uint64_t, uint64_t> res;
+                for(;;)
+                {
                     uint64_t nok = 0;
                     {
                         unique_lock<mutex> lock(mdm);
-                        mdc.wait(lock);
-
+                        if(md.empty())
+                        {
+                            mdc.wait(lock);
+                        }
+                        else if(stop)
+                        {
+                            return res;
+                        }
                         nok = md.front();
                         md.pop_front();
                     }
 
-                    // break;
+                    auto r = expand(nok);
+                    for(auto &prime: r)
+                    {
+                        res[prime]++;
+                    }
                 }
-
             });
         }
 
+        /*
+         * getting numbers from stdin
+         */
         string line;
         while (cin >> line)
         {
@@ -64,9 +126,35 @@ int main(int argc, char **argv)
             }
         }
 
-        for (auto &t : threads)
+        /*
+         * stopping all threads
+         */
+        stop = true;
+        mdc.notify_all();
+
+        /*
+         * collect all the eggs in one basket :)
+         */
+        map<uint64_t, uint64_t> nok;
+        for (auto &f : futures)
         {
-            t.join();
+            auto r = f.get();
+
+//            cout << "thread " << endl;
+            for (auto& p : r)
+            {
+//                cout << "prime is " << p.first << " power " << p.second << endl;
+                if(p.second > nok[p.first])
+                    nok[p.first] = p.second;
+            }
+        }
+
+        /*
+         * print result
+         */
+        cout << "lcm" << endl;
+        for(auto &n: nok){
+            cout << "prime " << n.first << " power " << n.second << endl;
         }
     }
     catch (const std::exception& e)
