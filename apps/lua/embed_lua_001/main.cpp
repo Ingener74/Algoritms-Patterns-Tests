7a5af1099801568ad573616be57246eb9805138f
Shnayder Pavel <pavel_shnayder@3divi.com>
2014-06-06 08:31:40 +0600

    added lua file in embedding lua i now can call C function

--- apps/lua/embed_lua_001/main.cpp
+++ apps/lua/embed_lua_001/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <string>
+#include <fstream>
 #include <iostream>
 #include <stdexcept>
 
@@ -16,10 +17,25 @@ using namespace std;
 extern "C"
 {
 
-static int l_testLut(lua_State* L)
-{
-    return 128;
-}
+    static int l_testLua(lua_State* L)
+    {
+        double t = lua_tonumber(L, 1);
+        lua_pushnumber(L, t * 3.141592);
+        return 1;
+    }
+
+    static int l_testString(lua_State* L){
+        lua_pushstring(L, "This string returned from C function");
+        return 1;
+    }
+
+    static int l_new_print(lua_State* L){
+        for(int i = 1; i < lua_gettop(L) + 1; ++i){
+            cout << lua_tostring(L, i);
+        }
+        cout << endl;
+        return 0;
+    }
 
 }
 
@@ -27,21 +43,41 @@ int main(int argc, char **argv)
 {
     try
     {
-        cout << "Lua embedding tests" << endl;
+        if (argc < 2) throw runtime_error(
+                "\nUsage: ./embed_lua <path-to-lua-script>");
+
+        cout << "Lua embedding tests" << endl << endl;
 
         lua_State *L = luaL_newstate();
 
-        string text = ""
-                "local function main() "
-                "   print('Lua test')  "
-                "end                   "
-                "main()                ";
+        fstream file(argv[1]);
+        string text((std::istreambuf_iterator<char>(file)),
+                std::istreambuf_iterator<char>());
+
+        cout << "Lua script source code" << endl << endl;
+
+        cout << text << endl;
+
+        cout << "Script result" << endl << endl;
 
         luaopen_io(L);
+        luaopen_os(L);
         luaopen_math(L);
         luaopen_base(L);
+        luaopen_debug(L);
         luaopen_table(L);
         luaopen_string(L);
+        luaopen_package(L);
+        luaopen_coroutine(L);
+
+        lua_pushcfunction(L, l_testLua);
+        lua_setglobal(L, "test_lua");
+
+        lua_pushcfunction(L, l_testString);
+        lua_setglobal(L, "testString");
+
+        lua_pushcfunction(L, l_new_print);
+        lua_setglobal(L, "new_print");
 
         int res = luaL_loadstring(L, text.c_str());
         if (!res)
---
+++ apps/lua/embed_lua_001/test.lua
@@ -0,0 +1,12 @@
+-- local function main()
+-- end
+-- main()
+
+local old_print = print
+print = function(...)
+  new_print(...);
+end
+
+print('Test Lua Script');
+print('Test number from C function = ', test_lua(3), ' = 3 * 3.1415926');
+print("Test string from C function = ", testString());
