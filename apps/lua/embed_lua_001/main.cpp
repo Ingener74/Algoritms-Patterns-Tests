6b41dd2a896090448d717cd12927dc43cae7ff8f
Shnayder Pavel <pavel_shnayder@3divi.com>
2014-06-09 11:39:01 +0600

    fix and refactor lua

--- apps/lua/embed_lua_001/main.cpp
+++ apps/lua/embed_lua_001/main.cpp
@@ -14,6 +14,12 @@
 
 using namespace std;
 
+/*
+ * helpful
+ *
+ * * https://code.google.com/p/zester/wiki/Lua_C
+ */
+
 extern "C"
 {
 
@@ -31,7 +37,7 @@ extern "C"
 
     static int l_new_print(lua_State* L){
 
-        cout << "From lua print: ";
+//        cout << "From lua print: ";
 
         for(int i = 1; i < lua_gettop(L) + 1; ++i){
             cout << lua_tostring(L, i);
@@ -52,28 +58,20 @@ int main(int argc, char **argv)
         cout << "Lua embedding tests" << endl << endl;
 
         lua_State *L = luaL_newstate();
+        luaL_openlibs(L);
 
         fstream file(argv[1]);
         string text((std::istreambuf_iterator<char>(file)),
                 std::istreambuf_iterator<char>());
 
-        cout << "Lua script source code" << endl << endl;
-
-        cout << text << endl;
-
-        cout << "Script result" << endl << endl;
-
-        luaL_openlibs(L);
-
         lua_register(L, "testMul2Pi",   l_testMul2Pi);
         lua_register(L, "testString", l_testString);
         lua_register(L, "new_print",  l_new_print);
 
-
         int res = luaL_loadstring(L, text.c_str());
         if (!res)
         {
-            res = lua_pcall(L, 0, LUA_MULTRET, 0);
+            res = lua_pcall(L, 0, 0, 0);
         }
 
         lua_getglobal(L, "testLuaFunc");
--- apps/lua/embed_lua_001/test.lua
+++ apps/lua/embed_lua_001/test.lua
@@ -1,14 +1,12 @@
--- local function main()
--- end
--- main()
-
 local old_print = print
 print = function(...)
   new_print(...);
 end
 
+a = 12;
+
 print('Test Lua Script');
-print('Test number from C function = ', testMul2Pi(3), ' = 3 * 3.1415926');
+print('Test number from C function = ', testMul2Pi(a), ' = ', a, ' * 3.1415926');
 print("Test string from C function = ", testString());
 
 function testLuaFunc()
