804d831cd5b26168f365ada8b855f789098fbc67
Shnayder Pavel <pavel_shnayder@3divi.com>
2014-06-09 15:43:07 +0600

    learn lua OOP

--- apps/lua/embed_lua_001/test.lua
+++ apps/lua/embed_lua_001/test.lua
@@ -26,3 +26,24 @@ end
 function testSetInLua(new_b)
   b = new_b
 end
+
+-- http://ilovelua.narod.ru/about_lua.html#Methods
+lamp = {
+  on = 0;
+}
+function lamp:Off()
+  self.on = 0;
+end
+
+function lamp:On()
+  self.on = 1;
+end
+
+lamp_1 = {
+}
+
+setmetatable(lamp_1, {__index = lamp});
+lamp_1:On();
+
+print('lamp ', lamp_1.on);
+
