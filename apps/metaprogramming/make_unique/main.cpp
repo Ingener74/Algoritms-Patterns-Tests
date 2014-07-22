89312622d06fe705a2c72a769e428e0491c33fd7
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-07-23 00:51:25 +0600

    refactor

--- apps/metaprogramming/make_unique/main.cpp
+++ apps/metaprogramming/make_unique/main.cpp
@@ -15,45 +15,45 @@ using namespace std;
 class TestClass
 {
 public:
-	TestClass( int i = 0 ) :
-			_i( i )
-	{
-	}
-	virtual ~TestClass()
-	{
-	}
+    TestClass( int i = 0 ) :
+            _i(i)
+    {
+    }
+    virtual ~TestClass()
+    {
+    }
 
-	friend inline ostream& operator<<( ostream& out, const TestClass& rho );
+    friend inline ostream& operator<<( ostream& out, const TestClass& rho );
 
 private:
-	int _i = 0;
+    int _i = 0;
 };
 
 ostream& operator<<( ostream& out, const TestClass& rho )
 {
-	out << "Test class " << rho._i;
-	return out;
+    out << "Test class " << rho._i;
+    return out;
 }
 
-template<typename T, typename ... Args>
+template <typename T, typename ... Args>
 unique_ptr<T> make_unique( Args ... args )
 {
-	unique_ptr<T> ptr( new T( forward<Args>(args)... ) );
-	return ptr;
+    unique_ptr<T> ptr(new T(forward<Args>(args)...));
+    return ptr;
 }
 
 int main( int argc, char **argv )
 {
-	try
-	{
-		auto uptr = make_unique<TestClass>( 10 );
+    try
+    {
+        auto uptr = make_unique<TestClass>(10);
 
-		cout << *uptr << endl;
-	}
-	catch ( exception const & e )
-	{
-		cerr << "Error: " << e.what() << endl;
-	}
-	return 0;
+        cout << *uptr << endl;
+    }
+    catch ( exception const & e )
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
+    return 0;
 }
 
