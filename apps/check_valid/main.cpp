d35f050c8393ca42b9a31e11e24e1fe6710863d7
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-08-05 00:22:23 +0600

    delete tabs

--- apps/check_valid/main.cpp
+++ apps/check_valid/main.cpp
@@ -14,31 +14,32 @@ using namespace std;
 
 string backtrace()
 {
-	return "";
+    return "";
 }
-template<typename T>
-T* check_valid(T* a)
+template <typename T>
+T* check_valid( T* a )
 {
-	if (a == nullptr) throw runtime_error(string("check_valid\n") + typeid(T).name() + " * == nullptr\n" + backtrace());
-	return a;
+    if ( a == nullptr ) throw runtime_error(
+            string("check_valid\n") + typeid(T).name() + " * == nullptr\n" + backtrace());
+    return a;
 }
 
-int main(int argc, char **argv)
+int main( int argc, char **argv )
 {
-	try
-	{
-		cout << "check valid" << endl;
+    try
+    {
+        cout << "check valid" << endl;
 
-		int a1 = 0;
-		int * a2 = check_valid(&a1);
+        int a1 = 0;
+        int * a2 = check_valid(&a1);
 
-		int * a3 = nullptr;
-		int * a4 = check_valid(a3);
-	}
-	catch ( exception const & e )
-	{
-		cerr << "Error: " << e.what() << endl;
-	}
-	return 0;
+        int * a3 = nullptr;
+        int * a4 = check_valid(a3);
+    }
+    catch ( exception const & e )
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
+    return 0;
 }
 
