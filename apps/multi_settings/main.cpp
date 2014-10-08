17315cbf1d7167dce58b5ec2874a63bd3fc0be92
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-10-08 12:21:57 +0400

    sfinae getter from boost property tree ready

--- apps/multi_settings/main.cpp
+++ apps/multi_settings/main.cpp
@@ -14,29 +14,7 @@
 #include <boost/property_tree/json_parser.hpp>
 
 using namespace std;
-
 namespace pt = boost::property_tree;
-
-/*
-template<typename T>
-auto get(const pt::ptree& p, const string& name, T& t) -> decltype(t.operator()(p.get<int>(name)), void())
-{
-    t(p.get<int>(name));
-}
-
-template<typename T>
-auto get(const pt::ptree& p, const string& name, T& t) -> decltype(t = p.get<T>(name), void())
-{
-    t = p.get<T>(name);
-}
-
-template<typename T, typename ... Args>
-void get(const pt::ptree& p, const string& name, T& t, Args&... args)
-{
-    get(p, name, t);
-    get(p, args...);
-}
-*/
 
 template<typename P, typename T>
 auto get(P&& p, const string& name, T&& t) -> decltype(t.operator()(p.get<int>(name)), void())
@@ -57,37 +35,40 @@ void get(P&& p, const string& name, T&& t, Args&&... args)
     get(p, args...);
 }
 
-class TestGetter
+class ptree_holder
 {
+    pt::ptree _p;
 public:
+    ptree_holder(pt::ptree p): _p(p){
+    }
+    virtual ~ptree_holder() = default;
+
     template<typename T>
-    T get(const string& name)
+    typename remove_reference<T>::type get(const string& name)
     {
-        T t;
+        using rfT = typename remove_reference<T>::type;
+
+        rfT t = _p.get<rfT>(name);
+
         return t;
     }
 };
 
 void foo(int i)
 {
-    cout << "i " << i << endl;
+    cout << "test_bind with bind " << i << endl;
 }
 
-template<typename F>
-void bar(F&& f){
-    f = 10;
-}
-
-class foo1{
+class bar{
 public:
-    foo1(){
+    bar(){
     }
 
-    foo1(const foo1&) = delete;
-    foo1& operator=(const foo1&) = delete;
+    bar(const bar&) = delete;
+    bar& operator=(const bar&) = delete;
 
     void operator()(int i){
-        cout << "call operator " << i << endl;
+        cout << "test_bind with custom functor " << i << endl;
     }
 };
 
@@ -95,12 +76,7 @@ int main(int argc, char **argv)
 {
     try
     {
-        int j = 20;
-        bar(j);
-
-        cout << "i " << j << endl;
-
-        foo1 f;
+        bar f;
 
         pt::ptree p;
 
@@ -110,24 +86,12 @@ int main(int argc, char **argv)
         string s;
         bool b = false;
 
-//        get(p, "test_int", i, "test_string", s, "test_bool", b);
-//        get(p, "test_int", i, "test_string", s, "test_bool", b, "test_bind", f);
-//
-//        auto f2 = [](int i){ cout << "f2 lambda " << i << endl; };
-//        get(p, "test_int", i, "test_string", s, "test_bool", b, "test_bind", f2);
-//
-//        auto f3 = bind(foo, placeholders::_1);
-//        get(p, "test_int", i, "test_string", s, "test_bool", b, "test_bind", f3);
+        ptree_holder pth(p);
 
-        TestGetter tg;
-        auto p1 = tg;
-
-        get(p1, "test_int", i, "test_string", s, "test_bool", b);
-        get(p1, "test_int", i, "test_string", s, "test_bool", b, "test_bind", f);
-
-        get(p1, "test_int", i, "test_string", s, "test_bool", b, "test_bind", [](int i){ cout << "f2 lambda " << i << endl; });
-
-        get(p1, "test_int", i, "test_string", s, "test_bool", b, "test_bind", bind(foo, placeholders::_1));
+        get(pth, "test_int", i, "test_string", s, "test_bool", b);
+        get(pth, "test_int", i, "test_string", s, "test_bool", b, "test_bind", f);
+        get(pth, "test_int", i, "test_string", s, "test_bool", b, "test_bind", [](int i){ cout << "test_bind with lambda " << i << endl; });
+        get(pth, "test_int", i, "test_string", s, "test_bool", b, "test_bind", bind(foo, placeholders::_1));
 
         cout << i << " " << s << " " << b << endl;
 
