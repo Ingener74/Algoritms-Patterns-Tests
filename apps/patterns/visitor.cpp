65ec06a836c1ecee7d4955d85902f7bfcfe1ca74
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-10-21 15:10:02 +0400

    added test for visitor pattern

---
+++ apps/patterns/visitor.cpp
@@ -0,0 +1,27 @@
+/*
+ * visitor.cpp
+ *
+ *  Created on: Oct 21, 2014
+ *      Author: pavel
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    try
+    {
+        cout << "visitor example" << endl;
+
+        return 0;
+    }
+    catch (exception const & e)
+    {
+        cerr << e.what() << endl;
+        return 1;
+    }
+}
+
+
+
