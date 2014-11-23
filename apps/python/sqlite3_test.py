2db19ba2321d8abea8036441a5a055928aea2863
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-11-23 20:33:59 +0300

    last

--- apps/python/sqlite3_test.py
+++ apps/python/sqlite3_test.py
@@ -7,46 +7,28 @@ import sqlite3
 
 def main():
 
-    con1 = sqlite3.connect("test.sqlite3")
+    db_connection = sqlite3.connect("test.sqlite3")
 
-    cur1 = con1.cursor()
+    db_cursor = db_connection.cursor()
 
-    # cur1.execute('''create table test_table(
-    #     ID      INT  PRIMARY KEY NOT NULL,
-    #     NAME    TEXT             NOT NULL,
-    #     AGE     INT              NOT NULL,
-    #     ADDRESS CHAR
-    #     )''')
-
-    # cur1.execute('''create table body_mass(
-    #     id   INT PRIMARY KEY NOT NULL,
-    #     date TEXT NOT NULL,
-    #     mass REAL NOT NULL
-    # )''')
-
-    # cur1.execute('''INSERT INTO test_table(ID, NAME, AGE, ADDRESS) VALUES(1, "Pavel", 28, "Proisvodstvennaja")''')
-    # cur1.execute('''INSERT INTO body_mass(id, date, mass) VALUES (1, "10.1.1-11.1.2014", 112.3)''')
-
-    # con1.commit()
-
-    cur1.execute('''SELECT * FROM peoples''')
+    db_cursor.execute('''SELECT * FROM peoples''')
 
     print ''
-    for i in cur1:
+    for i in db_cursor:
         for j in i:
             print j
 
-    cur1.execute('''SELECT * FROM body_mass''')
+    db_cursor.execute('''SELECT * FROM body_mass''')
 
     print ''
-    for i in cur1:
+    for i in db_cursor:
         for j in i:
             print j
 
     print ''
     print 'close connection'
 
-    cur1.close()
+    db_cursor.close()
 
     print "connect successful"
 
