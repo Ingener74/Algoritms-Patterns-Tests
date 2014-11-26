69e450a51b1279ea091fe27d1e010978af685e5e
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-11-26 20:35:08 +0300

    last

--- apps/python/vkontakte_test.py
+++ apps/python/vkontakte_test.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import vkontakte
+from vk import *
+
+class Test:
+    def __init__(self, A):
+        self._a = A
+
+    @property
+    def a(self):
+        return self._a
+
+    @a.setter
+    def a(self, value):
+        self._a = value
 
 def main():
-    vk = vkontakte.API()
+    vk = API()
+
+
 
 if __name__ == '__main__':
     main()
