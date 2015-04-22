4ffa0343525d76f9bdcdc888e6addb0a6b7b293b
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-04-22 14:32:55 +0300

    возвращает адрес файла для скачивания

--- apps/python/simple_browser.py
+++ apps/python/simple_browser.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+'''
+Usage: python browser.py http://sourceforge.net/projects/boost/files/boost/1.58.0/
+'''
+
+
+import sys
 from PySide.QtGui import QApplication
 from PySide.QtWebKit import QWebView
 
+class MyWebView(QWebView):
+    def __init__(self):
+        super(MyWebView, self).__init__()
+
+        self.load(sys.argv[1])
+        
+        self.page().setForwardUnsupportedContent(True)
+        self.page().unsupportedContent.connect(self.content)
+        
+    def content(self, reply):
+        print reply.request().url().toString()
+        raise SystemExit
+        
+
 def main():
-    import sys
     app = QApplication(sys.argv)
     
-    web = QWebView()
-    web.load('http://www.google.ru')
-#     web.load('test.html')
+    web = MyWebView()
     web.show()
     
     sys.exit(app.exec_())
