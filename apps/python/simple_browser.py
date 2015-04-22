c4e2488d2a2d2b9dc953d2ee9db4cb907df6f45e
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-04-22 23:55:55 +0400

    небольшие правки

--- apps/python/simple_browser.py
+++ apps/python/simple_browser.py
@@ -6,32 +6,33 @@
 Usage: python browser.py http://sourceforge.net/projects/boost/files/boost/1.58.0/
 '''
 
-
 import sys
 from PySide.QtGui import QApplication
 from PySide.QtWebKit import QWebView
+
 
 class MyWebView(QWebView):
     def __init__(self):
         super(MyWebView, self).__init__()
 
         self.load(sys.argv[1])
-        
+
         self.page().setForwardUnsupportedContent(True)
         self.page().unsupportedContent.connect(self.content)
-        
+
     def content(self, reply):
         print reply.request().url().toString()
         raise SystemExit
-        
+
 
 def main():
     app = QApplication(sys.argv)
-    
+
     web = MyWebView()
     web.show()
-    
+
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
